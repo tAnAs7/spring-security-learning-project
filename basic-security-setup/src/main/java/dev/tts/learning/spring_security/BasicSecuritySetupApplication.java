be777472c9b89fbdb019ca9dc22af98b48f0180c
Quang Tan <qtan.ng@gmail.com>
2025-04-21 14:52:34 +0700

    ++ update artifactId and application name

--- basic-security-setup/src/main/java/dev/tts/learning/spring_security/BasicSecuritySetupApplication.java
+++ basic-security-setup/src/main/java/dev/tts/learning/spring_security/BasicSecuritySetupApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSecurityApplication {
+public class BasicSecuritySetupApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityApplication.class, args);
+		SpringApplication.run(BasicSecuritySetupApplication.class, args);
 	}
 
 }
